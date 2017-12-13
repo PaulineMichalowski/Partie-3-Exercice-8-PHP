0a8c80984083f59550f50b50a8e8bc643643747d
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:16:47 +0100

    Création de l'exercice 8

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <?php
+      for($line_number = 200; $line_number >= 0; $line_number = $line_number - 12) {
+        echo 'Enfin ! ';
+      }
+    ?>
+  </body>
+</html>
